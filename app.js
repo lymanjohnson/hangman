2b8e80476e52587bc8be13e36607f53a50f3c073
lymanjohnson <lyman.johnson@gmail.com>
2017-08-23 19:01:15 -0400

    MVP

--- app.js
+++ app.js
@@ -23,17 +23,27 @@ app.engine('mustache', mustache());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
-let word = getNewWord()
-let lives = 8;
+let word;
+let lives;
 
 function getNewWord(){
-  let newWord = "hi";
   newWord = words[Math.floor(Math.random()*words.length)];
   return newWord
 }
 
+function startOver(req){
+  word = getNewWord();
+  lives = 8;
+  req.sessionStore.guesses = [];
+  req.sessionStore.wordArray = [...word];
+  req.sessionStore.visibleWord = [];
+  for (i=0;i<word.length;i++){
+    req.sessionStore.visibleWord.push("-");
+  }
+}
+
 app.get('/', function (req, res) {
-  res.render('index',{word:word,lives:lives})
+  res.render('index',{word:word,lives:lives,playerData:req.sessionStore})
 })
 
 app.get('/guess', function (req, res) {
@@ -41,13 +51,68 @@ app.get('/guess', function (req, res) {
 })
 
 app.post('/new', function(req,res) {
-  word = getNewWord();
-  lives = 8;
+  startOver(req);
   res.redirect('/');
 })
 
+function isAlreadyGuessed(req){
+  // console.log("isAlreadyGuessed called");
+  // console.log("req.sessionStore.guesses",req.sessionStore.guesses);
+  for (i=0;i<req.sessionStore.guesses.length;i++){
+    // console.log("\ti:",i);
+    // console.log("\treq.sessionStore.guesses[i]",req.sessionStore.guesses[i]);
+    // console.log("\treq.body.letter",req.body.letter);
+    // console.log("\treq.body.letter == req.sessionStore.guess[i]",req.body.letter == req.sessionStore.guesses[i]);
+
+    if (req.body.letter == req.sessionStore.guesses[i]){
+      // console.log("already guessed!");
+      return true;
+    }
+  }
+  // console.log("not already guessed!");
+  return false;
+}
+
+function isLetterCorrect(req){
+  let found = false;
+  console.log("isLetterCorrect called");
+  for (i=0;i<req.sessionStore.wordArray.length;i++){
+    console.log(i);
+    if (req.sessionStore.wordArray[i] == req.body.letter){
+      found = true;
+      console.log("true!");
+      req.sessionStore.visibleWord[i] = req.sessionStore.wordArray[i];
+    }
+  }
+  return found;
+}
+
 app.post('/guess',function(req,res){
-  console.log(req.body.letter);
+
+  if (isAlreadyGuessed(req)){
+    console.log("already guessed");
+  }
+
+  else if (isLetterCorrect(req)) {
+    console.log("letter is correct");
+    //turn over the correct letters
+  }
+
+  else {
+    req.sessionStore.guesses.push(req.body.letter);
+    lives -= 1;
+    if (lives <= 0) {
+      startOver(req);
+    }
+
+  }
+
+  res.redirect("/");
+
+  // console.log("guesses:",req.sessionStore.guesses);
+  // console.log("visibleWord:",req.sessionStore.visibleWord);
+  // console.log("wordArray:",req.sessionStore.wordArray);
+
 })
 
 app.listen(port, function () {
