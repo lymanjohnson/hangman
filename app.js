0ba5495d1548ccfcf526a9cc68494aed4dce1b1e
lymanjohnson <lyman.johnson@gmail.com>
2017-08-24 14:04:38 -0400

    Working again

--- app.js
+++ app.js
@@ -75,50 +75,45 @@ function startOver(req){
 
   playMessage = "Good luck!";
   let word = getNewWordLengthBetween(minLength,maxLength);
+  // console.log(word);
   req.sessionStore.word = word;
   req.sessionStore.lives = maxLives;
-  req.sessionStore.score = word*maxLives;
+  req.sessionStore.score = word.length*maxLives;
   req.sessionStore.guesses = [];
   req.sessionStore.wordArray = [...word];
   req.sessionStore.visibleWord = [];
   for (i=0;i<word.length;i++){
     req.sessionStore.visibleWord.push("-");
   }
+// console.log(req.sessionStore.word,req.sessionStore.lives,req.sessionStore.score);
+
 }
 
 app.get('/', function (req, res) {
   debugCount += 1;
-  // //console.log("Hit '/' for the",debugCount+"th time.");
-  // //console.log("\t Lives:",lives);
-  // //console.log("\t Word:",word);
-  // //console.log("\t wordArray:",req.sessionStore.wordArray);
-  // //console.log("\t visibleWord:",req.sessionStore.visibleWord);
-  // //console.log("\t guesses:",req.sessionStore.guesses);
-  // //console.log("First if: req.sessionStore.wordArray == [] || typeof req.sessionStore.wordArray === 'undefined'",req.sessionStore.wordArray == [] || typeof req.sessionStore.wordArray === 'undefined');
-  // //console.log("Second if: arraysAreEqual(req.sessionStore.wordArray,req.sessionStore.visibleWord):",arraysAreEqual(req.sessionStore.wordArray,req.sessionStore.visibleWord));
-  // //console.log("Third if: lives <= 0",lives <= 0);
   // If the word array is empty or non-existent, start a new game
   if (req.sessionStore.wordArray == [] || typeof req.sessionStore.wordArray === "undefined"){
-    // //console.log("First if");
+    // console.log("First if");
     playMessage = "Welcome! A new game!!"
+    // console.log(req.sessionStore.word,req.sessionStore.lives,req.sessionStore.score);
     res.render('index',{playerData:req.sessionStore,playMessage:playMessage})
   }
 
   // If the visible array is the same as the word array, you've won
   else if (arraysAreEqual(req.sessionStore.wordArray,req.sessionStore.visibleWord)){
-    // //console.log("Second if");
+    // console.log("Second if");
     res.render('win',{playerData:req.sessionStore,playMessage:playMessage})
   }
 
   // If you've run out of lives, you've lost
-  else if (lives <= 0) {
-    // //console.log("Third if");
+  else if (req.sessionStore.lives <= 0) {
+    // console.log("Third if");
     res.render('lose',{playerData:req.sessionStore,playMessage:playMessage})
   }
 
   // Otherwise, keep playing the game
   else {
-    // //console.log("Else");
+    // console.log("Else");
   res.render('index',{playerData:req.sessionStore,playMessage:playMessage})
   }
 })
@@ -134,44 +129,28 @@ app.post('/new', function(req,res) {
 })
 
 function isAlreadyGuessed(req){
-  // //console.log("isAlreadyGuessed called");
-  // //console.log("req.sessionStore.guesses",req.sessionStore.guesses);
   for (i=0;i<req.sessionStore.guesses.length;i++){
-    // //console.log("\ti:",i);
-    // //console.log("\treq.sessionStore.guesses[i]",req.sessionStore.guesses[i]);
-    // //console.log("\treq.body.letter",req.body.letter);
-    // //console.log("\treq.body.letter == req.sessionStore.guess[i]",req.body.letter == req.sessionStore.guesses[i]);
 
     if (req.body.letter == req.sessionStore.guesses[i]){
-      // //console.log("already guessed!");
       return true;
     }
   }
 
   for (i=0;i<req.sessionStore.visibleWord.length;i++){
-    // //console.log("\ti:",i);
-    // //console.log("\treq.sessionStore.guesses[i]",req.sessionStore.guesses[i]);
-    // //console.log("\treq.body.letter",req.body.letter);
-    // //console.log("\treq.body.letter == req.sessionStore.guess[i]",req.body.letter == req.sessionStore.guesses[i]);
 
     if (req.body.letter == req.sessionStore.visibleWord[i]){
-      // //console.log("already guessed!");
       return true;
     }
   }
 
-  // //console.log("not already guessed!");
   return false;
 }
 
 function isLetterCorrect(req){
   let found = false;
-  // //console.log("isLetterCorrect called");
   for (i=0;i<req.sessionStore.wordArray.length;i++){
-    // //console.log(i);
     if (req.sessionStore.wordArray[i] == req.body.letter){
       found = true;
-      // //console.log("true!");
       req.sessionStore.visibleWord[i] = req.sessionStore.wordArray[i];
     }
   }
@@ -181,12 +160,10 @@ function isLetterCorrect(req){
 app.post('/guess',function(req,res){
 
   if (isAlreadyGuessed(req)){
-    // //console.log("already guessed");
     playMessage = "You already guessed that";
   }
 
   else if (isLetterCorrect(req)) {
-    // //console.log("letter is correct");
     playMessage = "Nice!";
     //turn over the correct letters
   }
@@ -201,33 +178,10 @@ app.post('/guess',function(req,res){
 
   res.redirect("/");
 
-  // //console.log("guesses:",req.sessionStore.guesses);
-  // //console.log("visibleWord:",req.sessionStore.visibleWord);
-  // //console.log("wordArray:",req.sessionStore.wordArray);
 
 })
 
-// app.get("/win",function(req,res){
-//   if (req.sessionStore.wordArray == req.sessionStore.visibleWord){
-//     res.render('win',{word:word,lives:lives,playerData:req.sessionStore,playMessage:playMessage})
-//   }
-//   else {
-//     playMessage = "Cheater! That cost you a turn."
-//     lives -= 1;
-//     res.redirect("/")
-//   }
-// })
-//
-// app.get("/lose",function(req,res){
-//   if(lives <= 0){
-//     playMessage = "Good effort!"
-//   }
-//   else {
-//     playMessage = "Why did you give up? You still had lives left."
-//   }
-//   res.render('lose',{word:word,lives:lives,playerData:req.sessionStore,playMessage:playMessage})
-// })
 
 app.listen(port, function () {
-	  //console.log('Successfully started express application!');
+	  console.log('Successfully started express application!');
 })
