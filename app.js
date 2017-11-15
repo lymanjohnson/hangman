429fbafc3ed4002dce91834b3e6c52d20c2ec500
lymanjohnson <lyman.johnson@gmail.com>
2017-11-15 14:34:37 -0500

    Modifies app.listen line

--- app.js
+++ app.js
@@ -319,10 +319,13 @@ app.post('/guess',function(req,res){
 })
 
 
-app.listen(port, function () {
-	  console.log('Successfully started express application!');
-})
+// app.listen(port, function () {
+// 	  console.log('Successfully started express application!');
+// })
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 /////
 
